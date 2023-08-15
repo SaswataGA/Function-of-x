30be5faca7e53e520348ca866b91a51912fdc8d2
Saswata Ghosal <140143035+SaswataGA@users.noreply.github.com>
2023-08-15 09:42:02 +0600

    Add files via upload

---
+++ f(x) = x – 3   and  g(x) = x2 + 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x,fx,gx;
+    scanf("%d",&x);
+    fx= x-3;
+    gx=pow(x,2)+2;
+    printf("x=%d\n",x);
+    printf("f(x)=%d\n",fx);
+    printf("g(x)=%d\n",gx);
+}
+
